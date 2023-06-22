06fb49dac2a6831ede987c646846142fb1339dcb
DaffaPutra7 <putradika3604@gmail.com>
2023-06-22 15:06:20 +0700

    Melakukan cek apakah antiran kosong dan REAR yang berapa di posisi terakhir Array akan kembali pada posisi awal Array

--- CircularQueues_133/CircularQueues_133.cpp
+++ CircularQueues_133/CircularQueues_133.cpp
@@ -18,11 +18,24 @@ public:
 		cout << endl;
 
 		// Cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR - 1)) {
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
 		}
 
-
+		// Cek apakah antiran kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// Jika REAR berada di posisi terakhir array, kembali ke awal array 
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
+
 };
